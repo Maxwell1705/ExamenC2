e4b783d58e4d1951e55d99921f0334278cc1a69d
Maxwell1705 <contrerasmaximiliano72@gmail.com>
2022-10-26 13:06:48 -0600

    Boton Calcular

---
+++ js/appExa2.js
@@ -0,0 +1,40 @@
+function calcular(){
+    var can=document.getElementById('cantidad').value;
+    var ori=document.getElementById('monedaOrigen').value;
+    var des=document.getElementById('monedaDestino').value;
+    let subtotal=0.0;
+
+    if(ori==1 && des==2){
+        subtotal=can/(19.85);
+    } else if(ori==1 && des==3){
+        subtotal=(can/(19.85))*1.35;
+    } else if(ori==1 && des==4){
+        subtotal=(can/(19.85))*0.99;
+    } else if(ori==2 && des==1){
+        subtotal=can*19.85;
+    } else if(ori==2 && des==3){
+        subtotal=can*1.35;
+    } else if(ori==2 && des==4){
+        subtotal=can*.99;
+    } else if(ori==3 && des==1){
+        subtotal=(can/(1.3))*19.85;
+    } else if(ori==3 && des==2){
+        subtotal=can/(1.3);
+    } else if(ori==3 && des==4){
+        subtotal=(can/(1.3))*0.99;
+    } else if(ori==4 && des==1){
+        subtotal=(can/(0.99))*19.85;
+    } else if(ori==4 && des==2){
+        subtotal=(can/(0.99));
+    } else if(ori==4 && des==3){
+        subtotal=(can/(0.99))*1.35;
+    } else{
+        subtotal=0; 
+    }
+
+    document.getElementById("subtotal").value = subtotal.toFixed(2);
+    document.getElementById("Tcomision").value = (subtotal*0.03).toFixed(2);
+    document.getElementById("Tpagar").value = (subtotal-(subtotal*0.03)).toFixed(2); 
+
+    return subtotal;
+ }
